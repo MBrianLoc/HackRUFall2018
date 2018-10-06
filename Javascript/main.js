12a4d3865b165745e33b6deb59eba8b419f11bd4
Joshuaolazo <joshuaolazo@gmail.com>
2018-10-06 16:51:46 -0400

    Merge branch 'master' of https://github.com/MBrianLoc/HackRUFall2018

--- Javascript/main.js
+++ Javascript/main.js
@@ -2,10 +2,13 @@ var game = new Phaser.game(640, 360, Phaser.AUTO);
 
 var GameState = {
     preload: function(){
+        game.load.image('space', 'assets/background.jpg');
 
     },
     create: function(){
+        var s = game.add.sprite(80, 0, 'space');
 
+        s.rotation = 0.14;
     },
     update: function(){
 
