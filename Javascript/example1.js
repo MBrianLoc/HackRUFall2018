9a3737787b1a7f2a3403138e597c473bbb75c40d
BuildTools <unconfigured@null.spigotmc.org>
2018-10-08 18:14:32 -0400

    changed assets added all around asteroids

--- Javascript/example1.js
+++ Javascript/example1.js
@@ -7,79 +7,83 @@ class example1 extends Phaser.Scene {
     preload()
     {
 
-        this.load.image('astroid', 'Assets/astroid.png');
+        this.load.image('asteroid', 'Assets/asteroid.png');
         this.load.image('enemy', 'Assets/bullet.png');
         this.load.image('bullet', 'Assets/bullet.png');
         this.load.audio('pew', 'Assets/pew.mp3');
         this.load.audio('ow', 'Assets/ow.mp3');
+        this.load.image('ship', 'Assets/ship.png');
+        this.load.image('background', 'Assets/background.jpg');
 
     }
 
     create()
     {
-
+        this.add.image(1000,400,'background');
+        this.add.image(0,400,'background');
+        this.add.image(3000,400,'background');
+        this.add.image(1000,800,'background');
+        this.add.image(0,800,'background');
+        this.add.image(3000,800,'background');
+        spawnTimer = this.time.addEvent({ delay: 150, callback: spawnAsteroid, callbackScope: this, repeat: 1});
         var scene = this;
-        this.image = this.add.image(300,400,'astroid');
-        var enemy = this.physics.add.group({
-            key: 'bullet',
-            repeat: 11,
-            setXY: { x: 1500, y: 0, stepY: 70 },
-        });
-        enemy.children.iterate(function (child) {
+        player = this.physics.add.sprite(700,400,'ship');
+        enemies = this.physics.add.group();
+        /*enemies.children.iterate(function (child) {
 
-          child.setVelocityX(-20);
-
-        });
+          child.setVelocityX(Phaser.Math.RND.integerInRange(-20,-500));
+          child.setDisplaySize(80, 80);
+aw
+        });*/
+        this.cameras.main.setBounds(0, 0,16000, 16000);
+        this.cameras.main.startFollow(player);
         var bullets = this.physics.add.group();
         this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
-        this.soundFX = this.sound.add("pew");
+        this.sound.add("pew");
 
         this.input.on('pointerdown', function(event)
         {
-          var bullet = bullets.create(this.image.x, this.image.y, "bullet");
-          var xVel = (event.x-scene.image.x);
-          var yVel = (event.y-scene.image.y);
+          var bullet = bullets.create(player.x, player.y, "bullet");
+          var xVel = (event.x-player.x);
+          var yVel = (event.y-player.y);
           var speed = Math.sqrt(Math.pow(xVel,2) + Math.pow(yVel,2));
           bullet.setVelocity(xVel,yVel);
-          this.soundFX.play();
+          bullet.setDisplaySize(10, 10);
+          this.sound.play('pew');
         }, this);
 
 
-        this.input.keyboard.on('keyup', function(e)
-        {
-            if(e.key == "2")
-            {
-            }
-            if(e.key == "3")
-            {
-                this.scene.start("example3");
-            }
-        }, this)
-        this.physics.add.collider(bullets, enemy);
-        this.physics.add.collider(enemy, enemy);
+        this.physics.add.collider(bullets, enemies, enemyHit, null, this);
+        this.physics.add.overlap(player, enemies, playerHit, null, this);
+        this.physics.add.collider(enemies, enemies);
     }
 
     update(delta)
     {
         if(this.key_A.isDown)
         {
-            this.image.x--;
+            //player.setX(player.x - 1);
+            //player.setVelocityX(-160);
+            player.x = player.x - 2;
         }
         if(this.key_W.isDown)
         {
-            this.image.y--;
+            player.y = player.y - 2;
+            //player.setX(player.y - 1);
         }
         if(this.key_S.isDown)
         {
-            this.image.y++;
+            player.y = player.y + 2;
+            //player.setX(player.y + 1);
         }
         if(this.key_D.isDown)
         {
-            this.image.x++;
+            player.x = player.x + 2;
+            //player.setX(player.x + 1);
         }
     }
 }
--- Javascript/main.js
+++ Javascript/main.js
@@ -1,6 +1,6 @@
 var config = {
     type:Phaser.AUTO,
-    width:4000,
+    width:800,
     height:800,
     physics: {
         default:'arcade',
@@ -11,4 +11,54 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var player;
+var enemies;
+var spawnTimer;
+function enemyHit (bullet, enemies)
+{
+    enemies.disableBody(true, true);
+    bullet.disableBody(true,true);
+    this.sound.play('ow');
+}
+function playerHit (player, enemies)
+{
+    enemies.disableBody(true, true);
+    this.sound.play('ow');
+}
 
+function spawnAsteroid()
+{
+    rand = Phaser.Math.RND.integerInRange(1,4);
+    if(rand == 1)
+    {
+        x = Phaser.Math.RND.integerInRange(-200,1700);
+        y = -50;
+        yVel = Phaser.Math.RND.integerInRange(20,500);
+        xVel = Phaser.Math.RND.integerInRange(-500,500);
+    }
+    else if(rand == 2)
+    {
+        x = Phaser.Math.RND.integerInRange(-200,1700);
+        y = 850;
+        yVel = Phaser.Math.RND.integerInRange(-500,-20);
+        xVel = Phaser.Math.RND.integerInRange(-500,500);
+    }
+    else if(rand == 3)
+    {
+        x = -50;
+        y = Phaser.Math.RND.integerInRange(-100,900);
+        yVel = Phaser.Math.RND.integerInRange(-300,300);
+        xVel = Phaser.Math.RND.integerInRange(20,500);
+    }
+    else
+    {
+        x = -50;
+        y = Phaser.Math.RND.integerInRange(-100,900);
+        yVel = Phaser.Math.RND.integerInRange(-300,300);
+        xVel = Phaser.Math.RND.integerInRange(-500,-20);
+    }
+    var ast = enemies.create(x, y, 'asteroid');
+    ast.setVelocity(xVel,yVel);
+    ast.setDisplaySize(80, 80);
+    spawnTimer.reset({delay: 300, callback: spawnAsteroid, callbackScope: this, repeat: 1});
+}
