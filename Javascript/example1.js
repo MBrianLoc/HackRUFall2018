c30f6b22dc3a43c063f2803153be8a5c14f06240
Joshuaolazo <joshuaolazo@gmail.com>
2018-10-12 22:18:26 -0500

    fds
    fds

--- Javascript/example1.js
+++ Javascript/example1.js
@@ -1,3 +1,6 @@
+import Introid from 'Introid';
+import Charoid from 'Charoid';
+
 class example1 extends Phaser.Scene {
     constructor()
     {
@@ -6,67 +9,36 @@ class example1 extends Phaser.Scene {
 
     preload()
     {
-
-        this.load.image('astroid', 'Assets/astroid.png');
-        this.load.image('enemy', 'Assets/bullet.png');
-        this.load.image('bullet', 'Assets/bullet.png');
-        this.load.audio('pew', 'Assets/pew.mp3');
-        this.load.audio('ow', 'Assets/ow.mp3');
+        this.load.image('space', 'assets/background.png');
+        this.load.image('astroid', 'assets/astroid.png');
+        this.load.image('enemy', 'assets/bullet.png');
+        this.load.image('bullet', 'assets/bullet.png');
+        this.load.audio('pew', 'assets/pew.mp3');
+        this.load.audio('ow', 'assets/ow.mp3');
 
     }
 
     create()
     {
-        var scene = this;
-        this.image = this.add.image(300,400,'astroid');
-        var enemy = this.add.sprite(20,40,'enemy');
+        this.add.image(400, 500, 'space');
+        // Adding a group for Introids
+        let astroidGroup = this.game.add.group();
 
-        this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-        this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-        this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-        this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        // Adding a couple of Introids to the group so that we have a pool to spawn from (we're not using them all here)
+        for (let i = 0; i < 10; i++) {
+         astroidGroup.add(new Introid(this.game));
+        }
 
-        this.soundFX = this.sound.add("pew");
+        // Spawn a Introid of each type centered on 256x240 screen.
+        let  = IntroidGroup.getFirstExists(false); // get a Introid object
+        Introid.spawn(128, 120 - 32, "red"); // spawn that Introid
+        Introid = IntroidGroup.getFirstExists(false); // get a another Introid object
+        Introid.spawn(128, 120 + 32, "yellow"); // spawn that Introid
 
-        this.input.on('pointerdown', function(event)
-        {
-          var bullet = this.physics.add.image(this.image.x, this.image.y, "bullet");
-          var xVel = (event.x-scene.image.x);
-          var yVel = (event.y-scene.image.y);
-          var speed = Math.sqrt(Math.pow(xVel,2) + Math.pow(yVel,2));
-          bullet.setVelocity(xVel,yVel);
-          this.soundFX.play();
-        }, this);
-
-        this.input.keyboard.on('keyup', function(e)
-        {
-            if(e.key == "2")
-            {
-            }
-            if(e.key == "3")
-            {
-                this.scene.start("example3");
-            }
-        }, this)
     }
 
     update(delta)
     {
-        if(this.key_A.isDown)
-        {
-            this.image.x--;
-        }
-        if(this.key_W.isDown)
-        {
-            this.image.y--;
-        }
-        if(this.key_S.isDown)
-        {
-            this.image.y++;
-        }
-        if(this.key_D.isDown)
-        {
-            this.image.x++;
-        }
+
     }
 }
--- Javascript/main.js
+++ Javascript/main.js
@@ -1,14 +1,47 @@
+import Astroid from 'Astroid';
+import Introid from '/introid.js';
+import Charoid from '/charoid.js';
+
 var config = {
-    type:Phaser.AUTO,
-    width:4000,
-    height:800,
+    type: Phaser.AUTO,
+    width: 1080,
+    height: 1920,
     physics: {
-        default:'arcade',
+        default: 'arcade',
         arcade: {
+            gravity: { y: 0 }
         }
     },
-    scene: [example1, example2, example3]
+    scene: {
+        preload: preload,
+        create: create,
+    }
 };
 
+game.state.add('GameState', GameState);
+game.state.start('GameState');
 var game = new Phaser.Game(config);
 
+function preload ()
+{
+    this.load.image('space', 'assets/background.jpg');
+    this.load.image('astroid', 'assets/astroid.png');
+}
+
+
+function create ()
+{
+    var background = this.add.image(400, 400, 'space');
+    roids = this.game.add.group();
+
+
+
+
+}
+
+var time_til_spawn = Math.random()*3000 + 2000;  //Random time between 2 and 5 seconds.
+var last_spawn_time = game.time.time;
+
+function update() {  // This is your state's update loop
+
+}
